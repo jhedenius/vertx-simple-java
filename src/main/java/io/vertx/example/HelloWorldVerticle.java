1407f4979d1c34f3ac45d7e7b25818d0a8379e1b
joachimhedenius <joachim@hedenius.se>
2015-09-27 11:54:28 +0200

    cleanup

--- src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -1,16 +1,13 @@
 package io.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 
-/**
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
 public class HelloWorldVerticle extends AbstractVerticle {
 
-  @Override
-  public void start() {
-    // Create an HTTP server which simply returns "Hello World!" to each request.
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
-  }
+    @Override
+    public void start() {
+        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+        vertx.eventBus().consumer("test-channel", inboundMessage -> System.out.println("got message: " + inboundMessage.body()));
+        vertx.setPeriodic(1000, periodicContext -> vertx.eventBus().publish("test-channel", "Hello from Java verticle"));
+    }
 }
